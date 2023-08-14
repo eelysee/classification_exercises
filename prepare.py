0a778143916a4a77104e658cf63b997593a02436
Edwige Elysee <edwige.elysee@gmail.com>
2023-08-14 15:43:39 -0500

    decision tree

--- prepare.py
+++ prepare.py
@@ -1,6 +1,6 @@
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import acquire as ak 
 
 def prep_iris(i):
     a = i
@@ -10,8 +10,9 @@ def prep_iris(i):
     a = a.rename(columns ={'species_name':'species'})
     return a 
 
-def prep_titanic(i):
-    a = i
+def prep_titanic():
+    
+    a = ak.get_titanic_data()
     a = a.drop(columns=['passenger_id','embarked', 'deck', 'class'])
  
     return a 
@@ -31,3 +32,8 @@ def train_val_test(df, strat, seed = 55):
                                 random_state = seed,
                                 stratify = val_test[strat])
     return train, val, test
+
+
+def ret_dum(x):
+    x = pd.get_dummies(x)
+    return x
