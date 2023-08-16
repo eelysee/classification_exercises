d45d3ace2055ee84bcfc91816c6c1f98db374925
Edwige Elysee <edwige.elysee@gmail.com>
2023-08-16 12:33:20 -0500

    logistic regression excersisses

--- prepare.py
+++ prepare.py
@@ -24,6 +24,9 @@ def prep_telco(i):
  
     return a 
 
+
+# Splits data into train test val
+# strat should be my target variable in quotatoin marks
 def train_val_test(df, strat, seed = 55):
     train, val_test = train_test_split(df, train_size = 0.7,
                                       random_state = seed,
@@ -34,6 +37,51 @@ def train_val_test(df, strat, seed = 55):
     return train, val, test
 
 
+# Get dummy where x is dataframe 
 def ret_dum(x):
     x = pd.get_dummies(x)
     return x
+
+
+def theometrics(TP, TN, FP, FN):
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    recall = TP / (TP + FN)
+    true_positive_rate = TP / (TP + FN)
+    false_positive_rate = FP / (FP + TN)
+    true_negative_rate = TN / (TN + FP)
+    false_negative_rate = FN / (FN + TP)
+    precision = TP / (TP + FP)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    support = TP + FN
+    data = {
+        'Metric': ['Accuracy', 'Recall', 'True Positive Rate', 'False Positive Rate', 'True Negative Rate', 'False Negative Rate', 'Precision', 'F1-Score', 'Support'],
+        'Value': [accuracy, recall, true_positive_rate, false_positive_rate, true_negative_rate, false_negative_rate, precision, f1_score, support]
+    }
+    theometrics = pd.DataFrame(data)
+    return theometrics
+
+
+def cm_funk(y_train,y_train_pred):
+    cm = confusion_matrix(y_train, y_train_pred)
+    tn, fp, fn, tp = cm.ravel()
+
+    accuracy = (tp + tn)/(tn + fp + fn + tp)
+
+    true_positive_rate = tp/(tp + fn)
+    false_positive_rate = fp/(fp + tn)
+    true_negative_rate = tn/(tn + fp)
+    false_negative_rate = fn/(fn + tp)
+
+    precision = tp/(tp + fp)
+    recall = tp/(tp + fn)
+    f1_score = 2*(precision*recall)/(precision+recall)
+
+    support_pos = tp + fn
+    support_neg = fp + tn
+
+    output = {
+    'metric' : ['accuracy', 'true_positive_rate', 'false_positive_rate', 'true_negative_rate', 'false_negative_rate', 'precision', 'recall', 'f1_score', 'support_pos', 'support_neg']
+    ,'score' : [accuracy, true_positive_rate, false_positive_rate, true_negative_rate, false_negative_rate, precision, recall, f1_score, support_pos, support_neg]
+}
+
+    return pd.DataFrame(output)
